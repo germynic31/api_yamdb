de90a1dd315997ddd3b2c63fb4239314fcd4f6ce
german deev <geradeev57@gmail.com>
2024-02-25 14:51:11 +0300

    create serializers, views, urls for Title, Category, Genre. create import_data_from_csv

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path, include
 from rest_framework import routers
 
+from .views import TitleViewSet, GenreViewSet, CategoryViewSet
+
 
 router_v1 = routers.DefaultRouter()
+router_v1.register('titles', TitleViewSet, basename='titles')
+router_v1.register('genres', GenreViewSet, basename='genres')
+router_v1.register('categories', CategoryViewSet, basename='categories')
 
 
 urlpatterns = [
--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -21,11 +21,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    
-
 
     'rest_framework',
     'rest_framework_simplejwt',
+    'django_filters',
+
     'user.apps.UserConfig',
     'api.apps.ApiConfig',
     'reviews.apps.ReviewsConfig',
@@ -111,3 +111,9 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = ((BASE_DIR / 'static/'),)
 
 AUTH_USER_MODEL = 'user.MyUser'
+
+
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 20,
+}
