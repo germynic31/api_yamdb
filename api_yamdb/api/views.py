d49741f70c8a04d494282ca9dc42ba274731b5aa
Алексей Попов <vechanka@yandex.ru>
2024-02-27 20:09:00 +0300

    confirmation_code

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.tokens import default_token_generator
 from django.core.mail import send_mail
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import viewsets
@@ -53,10 +54,11 @@ class EmailViewSet(viewsets.ModelViewSet):
     permission_classes = (IsAuthenticated, )
 
     def mail(self):
+        token = default_token_generator.make_token(self.request.user)
 
         send_mail(
             subject='Код подтверждения',
-            message='Тут будет токен подтверждения', #TODO придумать реализацию confirmation_token 
+            message=f'Токен для подтверждения: {token}',
             from_email='from@example.com',
             recipient_list=[self.request.user.email],
             fail_silently=True,
