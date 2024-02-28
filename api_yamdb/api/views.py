2904388d6b1aa59f6486b65584584bfb51b36221
Алексей Попов <vechanka@yandex.ru>
2024-02-28 20:08:59 +0300

    edit role in user

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,27 +1,14 @@
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import viewsets
 from rest_framework.pagination import LimitOffsetPagination
-from rest_framework.permissions import IsAdminUser
 
-from user.models import MyUser
 from reviews.models import Title, Category, Genre
 from .mixins import ListDestroyCreateMixin
 from .serializers import (
-    UserSerializer, CategorySerializer,
+    CategorySerializer,
     GenreSerializer, CreateUpdateDestroyTitleSerializer,
     ListRetrieveTitleSerializer,
 )
-
-
-class UserViewSet(viewsets.ModelViewSet):
-    queryset = MyUser.objects.all()
-    serializer_class = UserSerializer
-    permission_classes = (IsAdminUser, )
-
-    def get_permissions(self):
-        if self.action == 'retrieve':
-            return (ReadOnly(),) #TODO сделаю в другой ветке
-        return super().get_permissions()
 
 
 class TitleViewSet(viewsets.ModelViewSet):
--- api_yamdb/user/models.py
+++ api_yamdb/user/models.py
@@ -1,6 +1,31 @@
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, Permission
 from django.db import models
 from django.utils.translation import gettext_lazy as _
+
+
+class RoleManager(models.Manager):
+    use_in_migrations = True
+
+    def get_by_natural_key(self, name):
+        return self.get(name=name)
+
+
+class Role(models.Model):
+    name = models.CharField(_('name'), max_length=150, unique=True)
+    permissions = models.ManyToManyField(
+        Permission,
+        verbose_name=_('permissions'),
+        blank=True,
+    )
+
+    objects = RoleManager()
+
+    class Meta:
+        verbose_name = _('group')
+        verbose_name_plural = _('groups')
+
+    def __str__(self):
+        return self.name
 
 
 class MyUser(AbstractUser):
@@ -10,5 +35,13 @@ class MyUser(AbstractUser):
         blank=True,
         unique=True,
     )
+    role = models.ForeignKey(
+        Role,
+        verbose_name=_('roles'),
+        blank=True,
+        related_name="user_set",
+        related_query_name="user",
+        default='user',
+        on_delete=models.CASCADE
+    )
     bio = models.CharField(_('bio'), max_length=256, blank=True)
-    role = models.CharField(_('role'), max_length=128, default='user')
