7d7271bd4e5d5ea4e8bc133c5cb7733d53989ccd
Аня Зыбель <ann@zebel.by>
2024-02-28 16:03:45 +0300

    views Commit and Review, serializers, urls

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,3 +1,36 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from rest_framework import viewsets
+from rest_framework.pagination import LimitOffsetPagination
 
-# Create your views here.
+from .serializers import ReviewSerializer, CommentSerializer
+from .models import Title, Review
+
+
+class ReviewViewSet(viewsets.ModelViewSet):
+    serializer_class = ReviewSerializer
+    # permission_classes
+    pagination_class = LimitOffsetPagination
+
+    def get_queryset(self):
+        return get_object_or_404(
+            Title, id=self.kwargs.get('title_id')).reviews.all()
+
+    def perform_create(self, serializer):
+        title_id = self.kwargs.get('title_id')
+        title = get_object_or_404(Title, id=title_id)
+        serializer.save(title=title, author=self.request.user)
+
+
+class CommentViewSet(viewsets.ModelViewSet):
+    serializer_class = CommentSerializer
+    # permission_classes
+    pagination_class = LimitOffsetPagination
+
+    def get_queryset(self):
+        return get_object_or_404(
+            Review, id=self.kwargs.get('review_id')).comments.all()
+
+    def perform_create(self, serializer):
+        review_id = self.kwargs.get('review_id')
+        review = get_object_or_404(Review, id=review_id)
+        serializer.save(review=review, author=self.request.user)
