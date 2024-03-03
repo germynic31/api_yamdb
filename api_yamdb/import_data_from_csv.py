3a208c60377090dee95967aa32ffba4f34fa82b8
german deev <geradeev57@gmail.com>
2024-03-03 23:52:48 +0300

    Modified the file import_data_from_csv.py, I corrected the code using flake8 and made readme.md

--- api_yamdb/import_data_from_csv.py
+++ api_yamdb/import_data_from_csv.py
@@ -1,26 +1,26 @@
+import logging
 import os
+import csv
 
 import django
 from django.conf import settings
+from django.db import IntegrityError
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'api_yamdb.settings')
 if not settings.configured:
     django.setup()
 
-import csv
-import logging
-
-from django.db import IntegrityError
-from reviews.models import Title, Category, Genre, GenreTitle, Comment, Review
-from user.models import MyUser
-
 
 def import_data_from_csv(file_path, model):
-    DEBUG_MESSAGE = f'path: {file_path}, model: {model}'
+    from user.models import MyUser
+    from reviews.models import Category
+
+    debug_message = f'path: {file_path}, model: {model}'
+
     with open(file_path, 'r', encoding='utf-8') as file:
         reader = csv.DictReader(file)
         columns = reader.fieldnames
-        logging.debug('Началась загрузка csv файла\n' + DEBUG_MESSAGE)
+        logging.debug('Началась загрузка csv файла\n' + debug_message)
         for row in reader:
             data = {}
             for column in columns:
@@ -36,24 +36,29 @@ def import_data_from_csv(file_path, model):
                 model.objects.create(**data)
             except IntegrityError:
                 pass
-        logging.debug('Завершилась загрузка csv файла\n' + DEBUG_MESSAGE)
-
-
-paths_models = [
-    # Не менять порядок!!!
-    ('static/data/users.csv', MyUser),
-    ('static/data/genre.csv', Genre),
-    ('static/data/category.csv', Category),
-    ('static/data/titles.csv', Title),
-    ('static/data/genre_title.csv', GenreTitle),
-    ('static/data/review.csv', Review),
-    ('static/data/comments.csv', Comment),
-]
+        logging.debug('Завершилась загрузка csv файла\n' + debug_message)
 
 
 def main():
-    for path, model in paths_models:
-        import_data_from_csv(path, model)
+    from user.models import MyUser
+    from reviews.models import (
+        Title, Category, Genre, GenreTitle, Comment, Review
+    )
+
+    default_path = 'static/data/'
+    file_names_models = [
+        ('users.csv', MyUser),
+        ('genre.csv', Genre),
+        ('category.csv', Category),
+        ('titles.csv', Title),
+        ('genre_title.csv', GenreTitle),
+        ('review.csv', Review),
+        ('comments.csv', Comment),
+    ]
+
+    for file_name, model in file_names_models:
+        full_path = os.path.join(default_path, file_name)
+        import_data_from_csv(full_path, model)
 
 
 if __name__ == '__main__':
